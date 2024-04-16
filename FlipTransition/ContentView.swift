1ced8a0fbc663b99c0015225e5e796af01e60fc4
Elaidzha1940 <eli080293@gmail.com>
2024-04-16 19:53:23 +0300

    FlipTransition

---
+++ FlipTransition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlipTransition
+//
+//  Created by Elaidzha Shchukin on 16.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlipTransition/FlipTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipTransitionApp.swift
+//  FlipTransition
+//
+//  Created by Elaidzha Shchukin on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlipTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
